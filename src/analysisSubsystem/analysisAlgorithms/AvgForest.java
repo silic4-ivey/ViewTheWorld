742f5fef069367269a73b5dc56583c65c27c18cd
stefan <stefan@192.168.0.170>
2021-03-31 20:02:17 -0400

    Added AvgForest Analysis, PM2VsForest Analysis, and Pie chart

--- src/analysisSubsystem/analysisAlgorithms/AvgForest.java
+++ src/analysisSubsystem/analysisAlgorithms/AvgForest.java
@@ -1,11 +1,13 @@
 package analysisSubsystem.analysisAlgorithms;
 
+import analysisSubsystem.Data;
 import analysisSubsystem.Result;
 import analysisSubsystem.dataFetchingSubsystem.Reader;
 import frontEnd.selectionSubsystem.Selection;
 
 public class AvgForest implements Analysis {
 	private Reader reader;
+	private double[][] value;
 	private String[] parts = {"Average forest area (% of land area)"};
 	
 	public AvgForest() {
@@ -15,8 +17,26 @@ public class AvgForest implements Analysis {
 	
 	@Override
 	public Result calculate(Selection sel) {
-		// TODO Auto-generated method stub
-		return null;
+		Data forest = reader.readData(sel.getAnalysisIndicators()[0].toString(), sel);		
+		
+		double sum = 0;
+		
+		if (forest.getValue() != null) {
+			
+			value = new double[1][1];
+			
+			for (int i =0; i<forest.getValue().length; i++) {
+				sum = sum + forest.getValue()[i];
+				System.out.println(sum);
+			}
+			
+			value [0][0] = sum/forest.getValue().length/100;
+
+		}
+		
+		System.out.println(value[0][0]);
+
+		return new Result(sel.getAnalysisName(), parts, value, forest.getYears());
 	}
 
 }
--- src/frontEnd/viewers/PieChart.java
+++ src/frontEnd/viewers/PieChart.java
@@ -1,5 +1,15 @@
 package frontEnd.viewers;
 
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.BorderFactory;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.general.DefaultPieDataset;
+
 import analysisSubsystem.Result;
 import frontEnd.MainUI;
 
@@ -8,6 +18,20 @@ public class PieChart implements Viewer {
 	@Override
 	public void notify(Result res) {
 		MainUI mainDisplay = MainUI.getInstance();
-//		mainDisplay.display("PieChart", res);
+		final int PIE_VALUE_LOC = 0;
+		
+		var dataset = new DefaultPieDataset(); 
+		dataset.setValue( res.getAnalysisParts()[PIE_VALUE_LOC], res.getValue()[PIE_VALUE_LOC][PIE_VALUE_LOC]);
+		dataset.setValue("Other", 1-res.getValue()[PIE_VALUE_LOC][PIE_VALUE_LOC]);
+		 
+		JFreeChart pieChart = ChartFactory.createPieChart(res.getAnalysisName(),
+		dataset, true, true, false);
+
+		ChartPanel chartPanel = new ChartPanel(pieChart);
+		chartPanel.setPreferredSize(new Dimension(400, 300));
+		chartPanel.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
+		chartPanel.setBackground(Color.white);
+		mainDisplay.display(chartPanel);
+
 	}
 }

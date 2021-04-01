a06d40e4fef7c65c5f5200f5b60aa8245871db64
reesercollins <reesecollins6@gmail.com>
2021-03-31 21:11:31 -0400

    Added RGovtExpEduVsHealthExp functionality

--- src/analysisSubsystem/analysisAlgorithms/RGovtExpEduVsHealthExp.java
+++ src/analysisSubsystem/analysisAlgorithms/RGovtExpEduVsHealthExp.java
@@ -1,12 +1,13 @@
 package analysisSubsystem.analysisAlgorithms;
 
+import analysisSubsystem.Data;
 import analysisSubsystem.Result;
 import analysisSubsystem.dataFetchingSubsystem.Reader;
 import frontEnd.selectionSubsystem.Selection;
 
 public class RGovtExpEduVsHealthExp implements Analysis {
 	private Reader reader;
-	private String[] parts = {"Govt exp on education & Current health exp"};
+	private String[] parts = {"Total Expenditure on Education (% of GDP)", "Total Expenditure on Health (% of GDP)"};
 	
 	public RGovtExpEduVsHealthExp() {
 		this.reader = new Reader();
@@ -14,8 +15,16 @@ public class RGovtExpEduVsHealthExp implements Analysis {
 	
 	@Override
 	public Result calculate(Selection sel) {
-		// TODO Auto-generated method stub
-		return null;
+		Data edu = reader.readData(sel.getAnalysisIndicators()[0], sel);
+		Data health = reader.readData(sel.getAnalysisIndicators()[1], sel);
+		
+		double[][] value = null;
+		if (edu.getValue() != null && health.getValue() != null) {
+			value = new double[2][Math.min(edu.getValue().length, health.getValue().length)];
+			value[0] = edu.getValue();
+			value[1] = health.getValue();
+		}
+		return new Result(sel.getAnalysisName(), parts, value, edu.getYears());
 	}
 
 }

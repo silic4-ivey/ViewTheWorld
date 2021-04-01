42d9792ee297c439abb140378a1bebfded4bef94
Joud Abdulmajeed El-Shawa <jelshawa@uwo.ca>
2021-03-31 22:25:38 -0400

    removed unnecessary imports in Reader

--- src/analysisSubsystem/dataFetchingSubsystem/Reader.java
+++ src/analysisSubsystem/dataFetchingSubsystem/Reader.java
@@ -5,8 +5,6 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
 import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 import analysisSubsystem.Data;

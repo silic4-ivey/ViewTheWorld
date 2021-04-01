a6049b78f8590e4e2f68e4cd7dec81d6fa4ba355
Liam Vukasinovic <lvukasin@uwo.ca>
2021-03-31 23:17:41 -0400

    AvgGovtExpEdu analysis type

--- src/analysisSubsystem/analysisAlgorithms/AvgGovtExpEdu.java
+++ src/analysisSubsystem/analysisAlgorithms/AvgGovtExpEdu.java
@@ -1,21 +1,36 @@
 package analysisSubsystem.analysisAlgorithms;
 
+import analysisSubsystem.Data;
+
 import analysisSubsystem.Result;
 import analysisSubsystem.dataFetchingSubsystem.Reader;
 import frontEnd.selectionSubsystem.Selection;
 
 public class AvgGovtExpEdu implements Analysis {
 	private Reader reader;
-	private String[] parts = {"Average govt exp on education"};
+	private double[][] value;
+	private String[] parts = {"Average Government expenditure on education (% of GDP)"};
 	
 	public AvgGovtExpEdu() {
 		this.reader = new Reader();
 	}
 	
+	
 	@Override
 	public Result calculate(Selection sel) {
-		// TODO Auto-generated method stub
-		return null;
+		Data govExp = reader.readData(sel.getAnalysisIndicators()[0].toString(), sel);
+		double average = 0;
+		value = new double[1][1];
+		if (govExp.getValue() != null) {
+			
+			for (int k = 0; k < govExp.getValue().length;k++) {
+				average += average + govExp.getValue()[k];
+			}
+			
+		}
+		value[0][0] = average/govExp.getValue().length;
+		
+		return new Result(sel.getAnalysisName(),parts,value,govExp.getYears());
 	}
 
 }
